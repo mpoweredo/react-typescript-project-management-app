787ef4c66afbccce33f8211ef2599dda22d839c2
mpoweredo <mpow3r1337@gmail.com>
2022-08-09 18:55:03 +0200

    Added firebase to project, and updated .gitignore file

---
+++ src/data/firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };

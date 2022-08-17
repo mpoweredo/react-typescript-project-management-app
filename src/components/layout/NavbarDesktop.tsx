1b13d09e4e001d9bbe358a0f3993bd657335fac8
mpoweredo <mpow3r1337@gmail.com>
2022-08-17 16:57:59 +0200

    Changed UI

--- src/components/layout/NavbarDesktop.tsx
+++ src/components/layout/NavbarDesktop.tsx
@@ -1,8 +1,8 @@
 import ViewKanbanIcon from '@mui/icons-material/ViewKanban';
 
 const classes = {
-	sidebar: 'h-screen bg-[#1a1c1e] fixed rounded-r-xl w-56',
-	nav: 'py-12 px-4 w-full h-full',
+	sidebar: 'h-screen fixed rounded-r-xl w-56 py-8',
+	nav: 'py-12 px-4 w-full h-full bg-[#1a1c1e] rounded-r-2xl',
 	list: 'flex flex-col gap-4 items-center justify-center',
 	listItem: 'w-full h-auto flex items-center justify-center p-3 bg-[#303234] rounded-md self-center cursor-pointer',
 	listContainer: 'w-full h-auto flex items-center justify-start gap-2 text-[#85888d] font-medium text-lg',

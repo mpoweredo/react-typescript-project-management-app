98c7f770b4d2c8b80209dfd2670b637cb48afad0
mpoweredo <mpow3r1337@gmail.com>
2022-08-20 12:56:17 +0200

    Installed types for react beautiful dnd

--- src/components/kanban/Column.tsx
+++ src/components/kanban/Column.tsx
@@ -1,5 +1,6 @@
 import { Column as ColumnType, Task as TaskType } from '../../types/KanbanTypes';
 import Task from './Task';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 const classes = {
 	Column: 'w-[260px] h-auto bg-[#11111388] rounded-md px-4 py-4',

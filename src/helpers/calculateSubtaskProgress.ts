f0d808a265f99cd2283e7b210555dcef0df6c8e4
mpoweredo <mpow3r1337@gmail.com>
2022-09-01 17:31:13 +0200

    :art: Deleted unnecessary if check in calculateSubtask function :art:

--- src/helpers/calculateSubtaskProgress.ts
+++ src/helpers/calculateSubtaskProgress.ts
@@ -2,7 +2,7 @@ import { Subtask } from '../types/KanbanTypes';
 
 const calculateProgress = (subtasks: Subtask[]) => {
 	const subtasksLength = subtasks.length;
-	const completedSubtasksLength = subtasks.filter(({ isCompleted }) => isCompleted === true).length;
+	const completedSubtasksLength = subtasks.filter(({ isCompleted }) => isCompleted).length;
 	const percent = (Math.floor((completedSubtasksLength / subtasksLength) * 100)) || 0
 
 	return {

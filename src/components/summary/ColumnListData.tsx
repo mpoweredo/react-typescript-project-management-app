2b1f01e1bd8e108414db70b9b12b16d32415945b
mpoweredo <mpow3r1337@gmail.com>
2022-09-13 07:19:02 +0200

    :art: Improved structure of Code in Chart and added glowing effect on pervent value :art:

---
+++ src/components/summary/ColumnListData.tsx
@@ -0,0 +1,30 @@
+import { ChartData } from '../../types/ChartTypes';
+
+type Props = {
+	chartData: ChartData;
+};
+
+const classes = {
+	kanbanColumnsList: 'h-full w-full flex flex-col gap-5',
+	itemTitle: 'text-gray-50 font-semibold text-lg',
+	itemValue: 'text-indigo-300 font-semibold text-lg',
+	itemDataContainer: 'flex items-center justify-between w-full',
+};
+
+const ColumnListData = ({ chartData }: Props) => {
+	return (
+		<ul className={classes.kanbanColumnsList}>
+			{chartData.map((item, index) => (
+				<li key={`item-${index}`} className='flex gap-3 items-center w-full'>
+					<div className='w-5 h-5 rounded-sm' style={{ backgroundColor: item.color }}></div>
+					<div className={classes.itemDataContainer}>
+						<p className={classes.itemTitle}>{item.title}</p>
+						<p className={classes.itemValue}>{item.value}</p>
+					</div>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export default ColumnListData;
--- src/components/summary/PieChart.tsx
+++ src/components/summary/PieChart.tsx
@@ -1,8 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { PieChart as Chart, Pie, Cell, Sector } from 'recharts';
 import { calculateColumnsItems } from '../../helpers/calculateColumnItems';
 import { ProjectData } from '../../store/projectContext';
 import { ChartData } from '../../types/ChartTypes';
+import ColumnListData from './ColumnListData';
+
+const classes = {
+	chartDataContainer: 'h-full w-full flex gap-10 items-center',
+};
 
 const PieChart = () => {
 	const [chartData, setChartData] = useState<ChartData>([]);
@@ -17,33 +22,40 @@ const PieChart = () => {
 		}
 	}, [project]);
 
-	const shapepl = ({ cx, cy, fill, percent, innerRadius, outerRadius, startAngle, endAngle }: any) => {
+	const onPieEnter = useCallback((_: any, index: number) => setActiveIndex(index), [setActiveIndex]);
+
+	const onAnimationEndHandler = () => setDidAnimationEnded(true);
+
+	const renderActiveShape = ({ cx, cy, fill, percent, innerRadius, outerRadius, startAngle, endAngle }: any) => {
 		const completePercent = percent * 100;
 
 		return (
-			<g >
-				{didAnimationEnded && <text className={`font-extrabold text-xl`} x={cx} y={cy} dy={8} textAnchor='middle' fill={fill}>
-					{completePercent.toFixed()}%
-				</text>}
+			<g>
+				{didAnimationEnded && (
+					<text
+						className={`font-extrabold text-xl`}
+						style={{ textShadow: `${fill} 0px 0px 18px` }}
+						x={cx}
+						y={cy}
+						dy={8}
+						textAnchor='middle'
+						fill={fill}>
+						{completePercent.toFixed()}%
+					</text>
+				)}
 				<Sector cx={cx} cy={cy} innerRadius={innerRadius} outerRadius={outerRadius} startAngle={startAngle} endAngle={endAngle} fill={fill} />
 			</g>
 		);
 	};
 
-	const onPieEnter = (_: any, index: number) => {
-		setActiveIndex(index);
-	};
-
-	const onAnimationEndHandler = () => setDidAnimationEnded(true);
-
 	return (
-		<div className='h-full w-full flex gap-10 items-center'>
+		<div className={classes.chartDataContainer}>
 			<div>
 				<Chart width={160} height={160}>
 					<Pie
 						onAnimationEnd={onAnimationEndHandler}
 						activeIndex={activeIndex}
-						activeShape={shapepl}
+						activeShape={renderActiveShape}
 						stroke='none'
 						data={chartData}
 						innerRadius={40}
@@ -57,19 +69,7 @@ const PieChart = () => {
 					</Pie>
 				</Chart>
 			</div>
-			<div className='h-full flex w-full'>
-				{didAnimationEnded && <ul className='h-full w-full flex flex-col gap-5'>
-					{chartData.map((item, index) => (
-						<li key={`item-${index}`} className='flex gap-3 items-center w-full'>
-							<div className='w-5 h-5 rounded-sm' style={{ backgroundColor: item.color }}></div>
-							<div className='flex items-center justify-between w-full'>
-								<p className='text-gray-50 font-semibold text-lg'>{item.title}</p>
-								<p className='text-indigo-300 font-semibold text-lg'>{item.value}</p>
-							</div>
-						</li>
-					))}
-				</ul>}
-			</div>
+			<div className='h-full flex w-full'>{didAnimationEnded && <ColumnListData chartData={chartData} />}</div>
 		</div>
 	);
 };
